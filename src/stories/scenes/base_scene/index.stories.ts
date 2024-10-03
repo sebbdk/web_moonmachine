26cacc5d2f39de287ceabcaa362de699fb7bb31e
Sebastian Vargr <870110+sebbdk@users.noreply.github.com>
2024-10-03 14:27:30 +0200

    organize more better

--- src/stories/scenes/base_scene/index.stories.ts
+++ src/stories/scenes/base_scene/index.stories.ts
@@ -8,9 +8,18 @@ export default {
   argTypes: {}
 };
 
-export const Primary = AppStage.bind({});
+export const Demo = AppStage.bind({});
 
-Primary.args = {
+Demo.args = {
+  sceneSrc:"raw_scenes/sc02/Sc02_HTML5_Canvas.js",
+  composition: "F67B9604FC919F489D182E47F2203F03",
+  assetsPath: "raw_scenes/sc02/",
+  rootFunctionName: "Sc02_canvas_demo"
+};
+
+export const Scene02 = AppStage.bind({});
+
+Scene02.args = {
   sceneSrc:"raw_scenes/sc02/Sc02_HTML5_Canvas.js",
   composition: "F67B9604FC919F489D182E47F2203F03",
   assetsPath: "raw_scenes/sc02/",
