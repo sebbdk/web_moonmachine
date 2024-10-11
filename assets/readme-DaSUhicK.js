1728d5c367c2d3a21012501ac64fc4f80618693a
sebbdk <870110+sebbdk@users.noreply.github.com>
2024-10-11 12:13:57 +0000

    Deploying to gh-pages from @ sebbdk/web_moonmachine@fce65587e68e0ea21bbef1450a71f2924e4259cb 🚀

--- assets/readme-DaSUhicK.js
+++ assets/readme-DaSUhicK.js
@@ -1,4 +1,4 @@
-import"./compat.module-BUAWlI2n.js";import{l as c,b as u}from"./preact.module-CMRueUcp.js";import{M as m}from"./index-BkBXZXMS.js";import{useMDXComponents as s}from"./index-GSsdXuhh.js";import"./iframe-B_B4ceko.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DrFu-skq.js";var f=0;function e(i,n,d,g,h,p){n||(n={});var r,l,o=n;if("ref"in o)for(l in o={},n)l=="ref"?r=n[l]:o[l]=n[l];var t={type:i,props:o,key:d,ref:r,__k:null,__:null,__b:0,__e:null,__d:void 0,__c:null,constructor:void 0,__v:--f,__i:-1,__u:0,__source:h,__self:p};if(typeof i=="function"&&(r=i.defaultProps))for(l in r)o[l]===void 0&&(o[l]=r[l]);return c.vnode&&c.vnode(t),t}function a(i){const n=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",strong:"strong"},s(),i.components);return e(u,{children:[e(m,{title:"Readme"}),`
+import"./compat.module-BUAWlI2n.js";import{l as c,b as u}from"./preact.module-CMRueUcp.js";import{M as m}from"./index-BLkt-zwN.js";import{useMDXComponents as s}from"./index-GSsdXuhh.js";import"./iframe-CIcUyhZE.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DrFu-skq.js";var f=0;function e(i,n,d,g,h,p){n||(n={});var r,l,o=n;if("ref"in o)for(l in o={},n)l=="ref"?r=n[l]:o[l]=n[l];var t={type:i,props:o,key:d,ref:r,__k:null,__:null,__b:0,__e:null,__d:void 0,__c:null,constructor:void 0,__v:--f,__i:-1,__u:0,__source:h,__self:p};if(typeof i=="function"&&(r=i.defaultProps))for(l in r)o[l]===void 0&&(o[l]=r[l]);return c.vnode&&c.vnode(t),t}function a(i){const n=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",strong:"strong"},s(),i.components);return e(u,{children:[e(m,{title:"Readme"}),`
 `,e(n.h1,{id:"sofus--the-moonmachine",children:"Sofus & the Moonmachine"}),`
 `,e(n.p,{children:"If you found this then congrats, you are currently looking at the the restoration project for Sofus & the Moonmachine!"}),`
 `,e(n.p,{children:`The project is still in the early fases, but a contact email for feedback will be added later.
@@ -47,4 +47,8 @@ The aim of this project is to build a web port that can run as a native app on i
 `,e(n.p,{children:"Technical notes:"}),`
 `,e(n.ul,{children:[`
 `,e(n.li,{children:"How to grab the right movie clip instance? (instance_1??)"}),`
+`]}),`
+`,e(n.p,{children:"Known bugs:"}),`
+`,e(n.ul,{children:[`
+`,e(n.li,{children:"When switching between scenes the sound stops working until you reload the page"}),`
 `]})]})}function x(i={}){const{wrapper:n}=Object.assign({},s(),i.components);return n?e(n,Object.assign({},i,{children:e(a,i)})):a(i)}export{x as default};
