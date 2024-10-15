0ede0e2fd0065defa898f9f8a011d3349e1b8435
sebbdk <870110+sebbdk@users.noreply.github.com>
2024-10-15 12:24:14 +0000

    Deploying to gh-pages from @ sebbdk/web_moonmachine@fc8896d0c650a0e4d7b8042c033b56f59df2e004 🚀

---
+++ assets/log-Dx0IIIsQ.js
@@ -0,0 +1,21 @@
+import"./compat.module-BUAWlI2n.js";import{u as e}from"./jsxRuntime.module-ByLYBOty.js";import{M as r}from"./index-CULhbFDt.js";import{useMDXComponents as o}from"./index-GSsdXuhh.js";import{b as l}from"./preact.module-CMRueUcp.js";import"./iframe-sBsc-7rH.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DrFu-skq.js";function i(t){const n=Object.assign({h1:"h1",h3:"h3",ul:"ul",li:"li",p:"p"},o(),t.components);return e(l,{children:[e(r,{title:"Readme/Logs"}),`
+`,e(n.h1,{id:"dev-logs",children:"Dev Logs"}),`
+`,e(n.h3,{id:"-seb-monday-15-october-2024",children:[e("hr",{})," Seb, Monday 15. October 2024"]}),`
+`,e(n.ul,{children:[`
+`,e(n.li,{children:"The server managing the website was frozen, it is now functional again."}),`
+`,e(n.li,{children:["The first 10 scenes and the chapter scenes have been added",`
+`,e(n.ul,{children:[`
+`,e(n.li,{children:"They are none functional, but will load and run their animations"}),`
+`]}),`
+`]}),`
+`]}),`
+`,e(n.h3,{id:"-seb-monday-14-october-2024",children:[e("hr",{})," Seb, Monday 14. October 2024"]}),`
+`,e(n.p,{children:"I finished the basic milestones plan along with some initial dates for when things should be completed."}),`
+`,e(n.p,{children:"I also setup this log to document the status of the project."}),`
+`,e(n.p,{children:"For now this is mostly for my self and the game designer to reference but this may change in the future."}),`
+`,e(n.p,{children:"Week 3 goals:"}),`
+`,e(n.ul,{children:[`
+`,e(n.li,{children:"Migrate first 10 scenes badly (they just need to load basically)"}),`
+`,e(n.li,{children:"Implement none POC/hacked sound manager for SFX, scene music and Voice."}),`
+`,e(n.li,{children:"Fix/generalize how scenes are loaded more better"}),`
+`]})]})}function f(t={}){const{wrapper:n}=Object.assign({},o(),t.components);return n?e(n,Object.assign({},t,{children:e(i,t)})):i(t)}export{f as default};
