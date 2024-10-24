6256bd91fa1c70ba126a95cfb726cfc46e2e6aa5
Sebastian Vargr <870110+sebbdk@users.noreply.github.com>
2024-10-24 14:08:29 +0200

    improved hit detection and added missing tween to ship assembly game

--- src/stories/scenes/scene_loader/lib/sc5_injection.ts
+++ src/stories/scenes/scene_loader/lib/sc5_injection.ts
@@ -35,12 +35,9 @@ export function injectScene4Drag(sceneClip, sceneConfig:SceneConfig) {
 				//hit test
 				var bubble = sceneClip.getChildByName('bubble');
 				var partInside = bubble.getChildByName(activePart.name);
+				var pos = partInside.globalToLocal(evt.stageX, evt.stageY);
 
-				var pos = { x:evt.localX-partInside.x+partInside.regX, y: evt.localY-partInside.y+partInside.regY}
-
-				// we need to reference the hit area directly
-				// otherwise it wont work for some reason...
-				if(partInside.hitArea.hitTest(pos.x, pos.y)) {
+				if(partInside.hitTest(pos.x, pos.y)) {
 					console.log('IT IS THE THING!!!')
 					sceneClip.playSound2("Rocketpart_place.mp3");
 
@@ -60,11 +57,11 @@ export function injectScene4Drag(sceneClip, sceneConfig:SceneConfig) {
 
 				//if no hits, clean up
 				activePart.gotoAndStop(0);
-				activePart.x = activePart.startX;
-				activePart.y = activePart.startY;
 
-				// @TODO, use a tween here
-				//TweenLite.to(activePart, 0.5, { x:activePart.startX, y:activePart.startY});
+				createjs.Tween
+					.get(activePart)
+					.to({x:activePart.startX, y:activePart.startY}, 300);
+
 				sceneClip.playSound2("Rocketpart_drop.mp3");
 			}
 
@@ -79,7 +76,6 @@ export function injectScene4Drag(sceneClip, sceneConfig:SceneConfig) {
 		}
 
 		sceneClip.stage.addEventListener("stagemousemove", move);
-		sceneClip.stage.addEventListener("pressup", moveStop);
 
 		parts.forEach((partName, index) => {
 			var part = this.getChildByName(partName);
@@ -88,107 +84,26 @@ export function injectScene4Drag(sceneClip, sceneConfig:SceneConfig) {
 			part.startY = part.y;
 			part.addEventListener("mousedown", moveStart);
 
+			// we need to listen directly here
+			// since the even wont bubble when we change frame later to show the draggable part
+			part.children[0].addEventListener("pressup", moveStop);
+
+			// Add a hit box for the bubble
+			// this is needed since they have alpha = 0
 			var bubble = sceneClip.getChildByName('bubble');
 			var partInside = bubble.getChildByName(partName);
+			var hit = partInside.clone();
+			hit.alpha = 1;
+			hit.x = partInside.regX;
+			hit.y = partInside.regY;
+			partInside.hitArea = hit;
+			partInside.alpha = 1;
+			partInside.children[0].alpha = 0;
 
-			if(!partInside.hitArea) {
-				var hit = partInside.clone();
-				hit.alpha = 1;
-				hit.x = partInside.regX;
-				hit.y = partInside.regY;
-				partInside.hitArea = hit;
-				partInside.alpha = 1;
-				partInside.children[0].alpha = 0;
-
-				if(BOOK_CONFIG.debug) {
-					hit.alpha = 0.1;
-					partInside.addChild(hit)
-				}
-
-				this.stage.update(); 
+			if(BOOK_CONFIG.debug) {
+				hit.alpha = 0.1;
+				partInside.addChild(hit)
 			}
 		})
 	}
 }
-
-
-/*
-stop();
-
-Sc05.allowHint5 = true;
-
-var activePart:MovieClip = null;
-var placeCount:int = 0;
-
-function moveStart(evt:MouseEvent):void {
-	var part:MovieClip = evt.target as MovieClip;
-	part.offsetX = evt.localX;
-	part.offsetY = evt.localY;
-	part.gotoAndStop(2);
-	activePart = part;
-}
-
-function moveStop(evt:MouseEvent):void {
-	if(activePart) {
-		//hit test
-		var bubble:MovieClip = getChildByName('bubble') as MovieClip;
-		var partInside:MovieClip = bubble.getChildByName(activePart.name) as MovieClip;
-		
-		
-		trace(partInside.x, partInside.y, bubble.mouseX, bubble.mouseY)
-		if(partInside.hitTestObject(activePart)) {
-			PageContent.playSound2("Rocketpart_place.mp3");
-
-			partInside.alpha = 1;
-			removeChild(activePart);
-			activePart = null;
-			placeCount++;
-			
-			if(placeCount === parts.length) {
-				next(true);
-				autoNext();
-				stop();
-				looping = true;
-				Sc05.allowHint5 = false;
-			}
-			
-			return;
-		}
-		
-		//if no hits, clean up
-		activePart.gotoAndStop(1);
-		TweenLite.to(activePart, 0.5, { x:activePart.startX, y:activePart.startY});
-		PageContent.playSound2("Rocketpart_drop.mp3");
-	}
-	activePart = null;
-
-}
-
-function move(evt:MouseEvent):void {
-	if(activePart) {
-		activePart.x = this.mouseX - activePart.offsetX;		
-		activePart.y = this.mouseY - activePart.offsetY;
-	}
-}
-
-var parts = [
-	'top',
-	'bottom',
-	'spire',
-	'window',
-	'middle'
-];
-
-this.removeEventListener(MouseEvent.MOUSE_MOVE, move);
-this.addEventListener(MouseEvent.MOUSE_MOVE, move);
-
-for each(var partname:String in parts) {
-	var part:MovieClip = getChildByName(partname) as MovieClip;
-	part.startX = part.x;
-	part.startY = part.y;
-	part.removeEventListener(MouseEvent.MOUSE_DOWN, moveStart);
-	part.removeEventListener(MouseEvent.MOUSE_UP, moveStop);
-	part.addEventListener(MouseEvent.MOUSE_DOWN, moveStart);
-	part.addEventListener(MouseEvent.MOUSE_UP, moveStop);
-}
-*/
