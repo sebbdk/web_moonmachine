20c48f8d4acb26dee95313653acebe3ba809f5ab
sebbdk <870110+sebbdk@users.noreply.github.com>
2024-10-28 13:30:09 +0000

    Deploying to gh-pages from @ sebbdk/web_moonmachine@f28a3d9b50b2c1882f726d3d2f2f553db462ceb9 🚀

--- assets/docs-Db4cxgx4.js
+++ assets/docs-Db4cxgx4.js
@@ -1,4 +1,4 @@
-import"./compat.module-BUAWlI2n.js";import{u as n}from"./jsxRuntime.module-ByLYBOty.js";import{M as c}from"./index-BpccAt1P.js";import{useMDXComponents as r}from"./index-GSsdXuhh.js";import{b as o}from"./preact.module-CMRueUcp.js";import"./iframe-BstxPa-j.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DrFu-skq.js";function i(l){const e=Object.assign({h1:"h1",p:"p",h2:"h2",a:"a",ul:"ul",li:"li",h3:"h3"},r(),l.components);return n(o,{children:[n(c,{title:"Readme"}),`
+import"./compat.module-BUAWlI2n.js";import{u as n}from"./jsxRuntime.module-ByLYBOty.js";import{M as c}from"./index-CkcZLFGR.js";import{useMDXComponents as r}from"./index-GSsdXuhh.js";import{b as o}from"./preact.module-CMRueUcp.js";import"./iframe-BcEfMOgP.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DrFu-skq.js";function i(l){const e=Object.assign({h1:"h1",p:"p",h2:"h2",a:"a",ul:"ul",li:"li",h3:"h3"},r(),l.components);return n(o,{children:[n(c,{title:"Readme"}),`
 `,n(e.h1,{id:"sofus--the-moonmachine",children:"Sofus & the Moonmachine"}),`
 `,n(e.p,{children:"If you found this then congrats, you are currently looking at the restoration project for Sofus & the Moonmachine!"}),`
 `,n(e.p,{children:`The project is still in the early phases, but a contact email for feedback will be added later.
@@ -8,7 +8,7 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n(e.h2,{id:"current-project-status",children:"Current project status:"}),`
 `,n(e.p,{children:["See the milestones ",n(e.a,{href:"/docs/readme-logs--docs",children:"or the dev logs"})," for more details."]}),`
 `,n(e.h2,{id:"milestones",children:"Milestones:"}),`
-`,n(e.p,{children:["The currently worked on milestones are marked in ",n("span",{style:"color:blue",children:"blue"})," and completed ones are marked in ",n("span",{style:"color:green",children:"green"})]}),`
+`,n(e.p,{children:["The currently worked on milestones are marked in ",n("span",{style:"color:blue",children:"blue"}),", completed ones are marked in ",n("span",{style:"color:green",children:"green"})," and done but may need more work are ",n("span",{style:"color:orange",children:"orange"}),"."]}),`
 `,n(e.p,{children:"The project is split into 3 major milestones"}),`
 `,n(e.ul,{children:[`
 `,n(e.li,{children:[`
@@ -21,7 +21,9 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n(e.li,{children:[`
 `,n("span",{style:"color: green",children:"Architecture stabilized"}),`
 `]}),`
-`,n(e.li,{children:"Basic closed test release to Android and iOS"}),`
+`,n(e.li,{children:[`
+`,n("span",{style:"color: green",children:"iOS And Android building with Capacitor"}),`
+`]}),`
 `]}),`
 `]}),`
 `,n(e.li,{children:["Content complete (November 2024)",`
@@ -31,14 +33,15 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n(e.li,{children:"The game should be completable"}),`
 `,n(e.li,{children:"All UI should be implemented"}),`
 `,n(e.li,{children:"i18n / basic English language version working"}),`
+`,n(e.li,{children:"Closed alpha release to Android and iOS (moved from October, requires some administrative work from)"}),`
 `]}),`
 `]}),`
 `,n(e.li,{children:["Interaction complete (December 2024)",`
 `,n(e.ul,{children:[`
 `,n(e.li,{children:"All the bonus touch interactions and effects should be implemented"}),`
 `,n(e.li,{children:"Known bugs from November should be fixed"}),`
-`,n(e.li,{children:"Closed beta test released to iOS"}),`
-`,n(e.li,{children:"Closed beta test released to Android"}),`
+`,n(e.li,{children:"Closed beta release to iOS"}),`
+`,n(e.li,{children:"Closed beta release to Android"}),`
 `]}),`
 `]}),`
 `]}),`
@@ -103,7 +106,9 @@ The aim of this project is to build a web port that can run as a native app on i
 `]}),`
 `,n(e.li,{children:["General actions",`
 `,n(e.ul,{children:[`
-`,n(e.li,{children:"Drag & Drop logic"}),`
+`,n(e.li,{children:[`
+`,n("span",{style:"color:orange",children:"Drag & Drop logic (got it, but it's not generalized)"}),`
+`]}),`
 `,n(e.li,{children:[`
 `,n("span",{style:"color:green",children:"Swipe logic"}),`
 `]}),`
@@ -132,9 +137,20 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n(e.li,{children:[`
 `,n("span",{style:"color:blue",children:"POC stuff + "}),`
 `,n(e.ul,{children:[`
-`,n(e.li,{children:"Compile and run as iOS app"}),`
-`,n(e.li,{children:"Compile and run as Android app"}),`
-`,n(e.li,{children:"Accelerometer test"}),`
+`,n(e.li,{children:[`
+`,n("span",{style:"color: green",children:"Compile and run as iOS app"}),`
+`]}),`
+`,n(e.li,{children:[`
+`,n("span",{style:"color: green",children:"Compile and run as Android app"}),`
+`]}),`
+`,n(e.li,{children:["Accelerometer test",`
+`,n(e.ul,{children:[`
+`,n(e.li,{children:"iOS"}),`
+`,n(e.li,{children:"Android"}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
 `,n(e.li,{children:["Test release",`
 `,n(e.ul,{children:[`
 `,n(e.li,{children:"Setup iOS store app"}),`
@@ -143,8 +159,6 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n(e.ul,{children:[`
 `,n(e.li,{children:"iOS"}),`
 `,n(e.li,{children:"Android"}),`
-`]}),`
-`]}),`
 `]}),`
 `]}),`
 `]}),`
@@ -169,7 +183,9 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n("span",{style:"color:green",children:"Scene 08 Swipe away fire"}),`
 `]}),`
 `,n(e.li,{children:"Scene 09 Shake iPad to get part down"}),`
-`,n(e.li,{children:"Scene 10 Fix racer puzzle"}),`
+`,n(e.li,{children:[`
+`,n("span",{style:"color:green",children:"Scene 10 Fix racer puzzle"}),`
+`]}),`
 `]}),`
 `,n(e.p,{children:"Known Bugs:"}),`
 `,n(e.ul,{children:[`
@@ -186,6 +202,7 @@ The aim of this project is to build a web port that can run as a native app on i
 `,n(e.li,{children:"Scene music does not loop"}),`
 `,n(e.li,{children:"What is up with playSound2? (second channel to avoid overlap??)"}),`
 `,n(e.li,{children:"In scene 6 it looks like Sofus is falling out of the rocket when it is launched (probably a broken tween)"}),`
+`,n(e.li,{children:"Multiple scenes seem to have problem with animation paths once exported to canvas (probably a createjs issue...)"}),`
 `]}),`
 `,n(e.h3,{id:"early-completed-milestones",children:"Early completed Milestones:"}),`
 `,n(e.ul,{children:[`
---
+++ assets/preview-mn0kuCSK.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-tWPEHae5.js","./iframe-BcEfMOgP.js","./compat.module-BUAWlI2n.js","./preact.module-CMRueUcp.js","./index-CkcZLFGR.js","./_commonjsHelpers-BosuxZz1.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./iframe-BcEfMOgP.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-tWPEHae5.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
