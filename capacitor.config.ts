8eff18a6f52a0fe3f31e579cf5a6824d415a6ab0
Sebastian Vargr <870110+sebbdk@users.noreply.github.com>
2024-10-28 14:05:46 +0100

    added ios/android building using capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dk.sebb.moonmachine',
+  appName: 'Sofus & the Moonmachine',
+  webDir: 'storybook-static'
+};
+
+export default config;
